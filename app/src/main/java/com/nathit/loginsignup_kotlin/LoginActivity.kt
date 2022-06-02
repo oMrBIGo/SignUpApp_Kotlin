fd907ea585fe13018b9a7272bd416a35965f8be1
oMrBIGo <nathit.panrod1@gmail.com>
2022-06-03 00:01:36 +0700

    Nathit

--- app/src/main/java/com/nathit/loginsignup_kotlin/LoginActivity.kt
+++ app/src/main/java/com/nathit/loginsignup_kotlin/LoginActivity.kt
@@ -1,11 +1,38 @@
 package com.nathit.loginsignup_kotlin
 
+import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.nathit.loginsignup_kotlin.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    //ViewBinding การผูกView
+    private lateinit var binding:ActivityLoginBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+
+    //FirebaseAth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure = กำหนดค่า
+        //configure actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "Login"
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Logging In...")
+        progressDialog.setCanceledOnTouchOutside(false) //ปิดการสัมผัสหน้าจอแล้วเด้ง progressDialog ออก
     }
 }
