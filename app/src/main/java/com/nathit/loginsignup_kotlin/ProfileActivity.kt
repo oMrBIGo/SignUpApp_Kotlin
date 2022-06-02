55eb9c2d898b1247bb28b7676e8aeeef765c03cd
oMrBIGo <nathit.panrod1@gmail.com>
2022-06-03 01:19:01 +0700

    Nathit

--- app/src/main/java/com/nathit/loginsignup_kotlin/ProfileActivity.kt
+++ app/src/main/java/com/nathit/loginsignup_kotlin/ProfileActivity.kt
@@ -1,11 +1,56 @@
 package com.nathit.loginsignup_kotlin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.ActionBar
+import com.google.firebase.auth.FirebaseAuth
+import com.nathit.loginsignup_kotlin.databinding.ActivityProfileBinding
 
 class ProfileActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding: ActivityProfileBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Profile"
+
+        //init firebase auth
+        firebaseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        //handle click, logout
+        binding.logoutBtn.setOnClickListener {
+            firebaseAuth.signOut()
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        ///TODO("checkUser||เซ็คกผู้ใช้งาน")
+        //check user is logged in or not
+        val firebaseUser = firebaseAuth.currentUser
+        if (firebaseUser != null) {
+            //user not null, user in logged in
+            val email = firebaseUser.email
+            //set to text view
+            binding.emailTv.text = email
+        } else {
+            //user is null, user is not logged in
+            startActivity(Intent(this, LoginActivity::class.java))
+            finish()
+        }
     }
 }
--- app/src/main/java/com/nathit/loginsignup_kotlin/SignUpActivity.kt
+++ app/src/main/java/com/nathit/loginsignup_kotlin/SignUpActivity.kt
@@ -1,11 +1,109 @@
 package com.nathit.loginsignup_kotlin
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.google.firebase.auth.FirebaseAuth
+import com.nathit.loginsignup_kotlin.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+
+    //ViewBinding
+    private lateinit var binding: ActivitySignUpBinding
+
+    //ActionBar
+    private lateinit var actionBar: ActionBar
+
+    //ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+    //FirebaseAuth
+    private lateinit var firebaseAuth: FirebaseAuth
+    private var email = ""
+    private var  password = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Configure ActionBar
+        actionBar = supportActionBar!!
+        actionBar.title = "Sign Up"
+        actionBar.setDisplayHomeAsUpEnabled(true)
+        actionBar.setDisplayShowHomeEnabled(true)
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait")
+        progressDialog.setMessage("Creating account In...")
+        progressDialog.setCanceledOnTouchOutside(false) //ปิดการสัมผัสหน้าจอแล้วเด้ง progressDialog ออก
+
+        //init firebaseAuth
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //handle click, begin sign up
+        binding.SignUpBtn.setOnClickListener {
+            validateData()
+        }
+
+    }
+
+    private fun validateData() {
+        ///TODO("validateData||ตรวจสอบข้อมูลสมาชิก")
+        //get data
+        email = binding.emailEt.text.toString().trim()
+        password = binding.passwordEt.text.toString().trim()
+
+        //validate data
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            //invalid email format
+            binding.emailEt.error = "Invalid email format"
+        } else if (TextUtils.isEmpty(password)) {
+            //password isn't entered
+            binding.passwordEt.error = "Please enter password"
+        } else if (password.length <6){
+            //password length is less than 6
+            binding.passwordEt.error = "Password must atlas 6 characters long"
+        } else {
+            //date is valid, continue signup
+            firebaseSignUp()
+        }
+    }
+
+    private fun firebaseSignUp() {
+        ///TODO("firebaseSignUp||ลงทะเบียนผู้ใช้งานFirebase")
+        //show progress
+        progressDialog.show()
+
+        //create account
+        firebaseAuth.createUserWithEmailAndPassword(email, password)
+            .addOnSuccessListener {
+                //signup success
+                progressDialog.dismiss()
+                //get current user
+                val firebaseUser = firebaseAuth.currentUser
+                val email = firebaseUser!!.email
+                Toast.makeText(this, "Account created with email $email", Toast.LENGTH_SHORT).show()
+
+                //open profile
+                startActivity(Intent(this, ProfileActivity::class.java))
+                finish()
+            }
+            .addOnFailureListener {e->
+                //signup failed
+                progressDialog.dismiss()
+                Toast.makeText(this, "SignUp Failed due to ${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed() //go back to previous activity, when back button of actionbar clicked
+        return super.onSupportNavigateUp()
     }
 }
